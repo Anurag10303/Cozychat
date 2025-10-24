22dcb19c7e1722f6b6325ecf1d722c58bd0bf79f
Anurag10303 <anuragsingh22230@gmail.com>
2025-10-24 11:54:19 +0530

    changed base url

--- Backend/Frontend/src/config.js
+++ Backend/Frontend/src/config.js
@@ -1,10 +1,9 @@
 // src/baseURL.js
-const BASE_URL = "https://chat-application-j0m9.onrender.com"
-// const BASE_URL ="http://localhost:3000"
+// const BASE_URL = "https://chat-application-j0m9.onrender.com"
+const BASE_URL = "http://localhost:3000";
 
 export default BASE_URL;
-console.log("BASE",BASE_URL);
-console.log("This is import",import.meta.env.DEV,import.meta.env.production,)
+console.log("BASE", BASE_URL);
+console.log("This is import", import.meta.env.DEV, import.meta.env.production);
 console.log(import.meta.env);
 console.log(import.meta.env.VITE_API_URL);
-
