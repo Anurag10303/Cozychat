6d4fc66a36b63d8a525ef32a8537f75182a5d70f
Anurag10303 <anuragsingh22230@gmail.com>
2026-02-24 22:05:09 +0530

    fixed production issue

--- Backend/index.js
+++ Backend/index.js
@@ -1,71 +1,63 @@
-import express from "express";
 import mongoose from "mongoose";
-import userRoute from "./routes/user.route.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 import cookieParser from "cookie-parser";
+import express from "express";
+import { fileURLToPath } from "url";
+
+import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socketIo/server.js";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 dotenv.config();
+
 const Port = process.env.PORT || 4000;
 const URI = process.env.MONGODB_URL;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
 
-// allow both deployed and local frontend
-const allowedOrigins = [
-  "http://localhost:5173", // for local frontend
-  "https://cozychat1.onrender.com", // for deployed frontend
-];
-
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://cozychat1.onrender.com"],
-    // origin: "http://localhost:5173", // Match frontend origin
-    credentials: true, // Allow cookies
-    exposedHeaders: ["Set-Cookie", "Authorization"],
+    credentials: true,
   }),
 );
 
 app.use("/user", userRoute);
 app.use("/user/message", messageRoute);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// -------deployment----
+// ===== PRODUCTION FRONTEND SERVING =====
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
   const frontendPath = path.join(__dirname, "..", "Frontend", "dist");
 
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
-// ✅ Root test route (for development and backend testing)
-app.get("/", (req, res) => {
-  res.send("✅ CozyChat backend is live and working!");
+// Root test route
+app.get("/api-test", (req, res) => {
+  res.send("Backend running");
 });
 
 mongoose
   .connect(URI)
   .then(() => {
     console.log("MongoDB connected");
+    console.log("Connected DB:", mongoose.connection.name);
 
     server.listen(Port, () => {
       console.log(`Server running on port ${Port}`);
     });
-    console.log("Connected DB:", mongoose.connection.name);
   })
   .catch((error) => {
     console.error("MongoDB connection failed:", error);
